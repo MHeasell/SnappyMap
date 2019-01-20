39cfb37c5226175eac54b7969aca0078dfe0f8b0
Michael Heasell <michael.heasell@gmail.com>
2019-01-20 19:18:57 +0000

    Add appveyor config

---
+++ appveyor.bat
@@ -0,0 +1,21 @@
+cd "%APPVEYOR_BUILD_FOLDER%" || goto :error
+
+rem Use python 3, not python 2
+SET PATH=C:\Python36-x64;%PATH%
+
+nuget restore SnappyMap.sln || goto :error
+msbuild SnappyMap.sln /p:Configuration=%Configuration% /m /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || goto :error
+
+cd dist || goto :error
+IF "%Configuration%"=="Release" (
+    python MakeReleaseZip.py --release || goto :error
+) ELSE (
+    python MakeReleaseZip.py || goto :error
+)
+for %%i in (*.zip) do appveyor PushArtifact %%i
+
+goto :EOF
+
+
+:error
+exit /b %errorlevel%
